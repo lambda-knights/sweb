8e6966d9e753c71c78dd2f284ee8a8cb414d222e
Eduardo Acuña Yeomans <eduardo.acye@gmail.com>
2016-10-11 00:08:14 -0700

    Éxitos y fallos como vectores

--- bootstrap/lib/parcomb.scm
+++ bootstrap/lib/parcomb.scm
@@ -1,9 +1,4 @@
 #|
-
-# (`lib/parcomb.scm`)
-
-## ***Documentación***
-
 Un parser es un procedimiento que toma tres argumentos:
 
 1. Una entrada `in`
@@ -35,81 +30,23 @@ Los combinadores de parsers son procedimientos que toman como argumentos a parse
 
 |#
 
-#|
-
-## ***Implementación***
-
-|#
-
-#|
-
-### ***Parsers***
-
-`:succeed:`
- : siempre parsea la entrada correctamente sin consumirla y el *match* satisface `null?`
-
-`:fail:`
- : siempre parsea la entrada incorrectamente sin consumirla y el *why* es la cadena `"no reason"`
-
-`:empty:`
- : parsea correctamente solo las entradas vacías
-
-`:nempty`
- : parsea correctamente solo las entradas no vacías
-
-`(:pred: pred)`
- : parsea correctamente si el primer elemento de una entrada satisface `pred`
-
-`(:npred: pred)`
- : similar a `:pred:` pero con el predicado negado
-
-`((:cmp: =) x)`
- : produce creadores de parsers que comparan el primer elemento de una entrada con `x` bajo la relación `=`
-
-`((:ncmp: =) x)`
- : similar a `:cmp:` pero negando la relación `=`
-
-`(:equal: x)`
- : equivalente a `(:cmp: equal?)`
-
-`(:eqv: x)`
- : equivalente a `(:cmp: eqv?)`
-
-`(:eq: x)`
- : equivalente a `(:cmp: eq?)`
-
-`(:nequal: x)`
- : equivalente a `(:ncmp: equal?)`
-
-`(:neqv: x)`
- : equivalente a `(:ncmp: eqv?)`
-
-`(:neq: x)`
- : equivalente a `(:ncmp: eq?)`
-
-
-|#
-
 (define (:succeed: in ok bad)
   (ok (success '() in)))
-#||#
 
 (define (:fail: in ok bad)
   (bad (failure '() in)))
-#||#
 
 (define (:empty: in ok bad)
   (if (input-null? in)
       (:succeed: in ok bad)
-      (bad (failure (list (list "not an empty input"
-                                (list (cons "found:" (input-car in))))) in))))
-#||#
+      (bad (failure `(("not an empty input"
+                       (("found:" . ,(input-car in)))))
+                    in))))
 
 (define (:nempty: in ok bad)
   (if (input-null? in)
       (bad (failure '(("not expecting empty input")) in))
       (:succeed: in ok bad)))
-#||#
 
 (define ((:pred: pred) in ok bad)
   (cond ((input-null? in)
@@ -118,23 +55,22 @@ Los combinadores de parsers son procedimientos que toman como argumentos a parse
          (ok (success (list (input-car in))
                       (input-cdr in))))
         (else
-         (bad (failure (list (list "(pred x) => #f"
-                                   (list (cons "x is:" (input-car in))
-                                         (cons "pred is:" pred))))
+         (bad (failure `(("(pred x) => #f"
+                          (("x is:" . ,(input-car in))
+                           ("pred is:" . ,pred))))
                        in)))))
-#||#
 
 (define ((:npred: pred) in ok bad)
   (cond ((input-null? in)
          (:nempty: in ok bad))
         ((pred (input-car in))
-         (bad (failure (list (list "(pred x) => #t"
-                                   (list (cons "x is:" (input-car in))
-                                         (cons "pred is:" pred)))))))
+         (bad (failure `(("(pred x) => #t"
+                          (("x is:" . ,(input-car in))
+                           ("pred is:" . ,pred))))
+                       in)))
         (else
          (ok (success (list (input-car in))
                       (input-cdr in))))))
-#||#
 
 (define (((:cmp: =) x) in ok bad)
   (cond ((input-null? in)
@@ -143,55 +79,32 @@ Los combinadores de parsers son procedimientos que toman como argumentos a parse
          (ok (success (list (input-car in))
                       (input-cdr in))))
         (else
-         (bad (failure (list (list "x ≠ y"
-                                   (list (cons "x is:" x)
-                                         (cons "y is:" (input-car in))
-                                         (cons "= is:" =))))
+         (bad (failure `(("x ≠ y"
+                          (("x is:" . ,x)
+                           ("y is:" . ,(input-car in))
+                           ("= is:" . ,=))))
                        in)))))
-#||#
 
 (define :equal: (:cmp: equal?))
 (define :eqv: (:cmp: eqv?))
 (define :eq: (:cmp: eq?))
-#||#
 
 (define (((:ncmp: =) x) in ok bad)
   (cond ((input-null? in)
          (:nempty: in ok bad))
         ((= x (input-car in))
-         (bad (failure (list (list "x = y"
-                                   (list (cons "x is:" x)
-                                         (cons "y is:" (input-car in))
-                                         (cons "= is:" =))))
+         (bad (failure `(("x = y"
+                          (("x is:" . ,x)
+                           ("y is:" . ,(input-car in))
+                           ("= is:" . ,=))))
                        in)))
         (else
          (ok (success (list (input-car in))
                       (input-cdr in))))))
-#||#
 
 (define :nequal: (:ncmp: equal?))
 (define :neqv: (:ncmp: eqv?))
 (define :neq: (:ncmp: eq?))
-#||#
-
-#|
-
-### ***Combinadores básicos***
-
-`(:seq: pars ...)`
- : Parsea de manera secuencial la entrada utilizando sus argumentos en orden, corresponde a una concatenación o secuenciación de parsers
-
-`(:alt: pars ...)`
- : Parsea de manera condicional la entrada utilizando sus argumentos en orden, corresponde a una disyunción o alternativa de parsers y deja de parsear cuando uno de sus parsers es un éxito
-
-`(:*: par)`
- : Parsea cero o más veces la entrada utilizando el parser `par`
-
-`(:+: par)`
- : Parsea una o más veces la entrada utilizando el parser `par`
-
-
-|#
 
 (define (:seq: . pars)
   (define ((:seq2: par1 par2) in ok bad)
@@ -206,7 +119,6 @@ Los combinadores de parsers son procedimientos que toman como argumentos a parse
                     (bad f2))))
           bad))
   (fold-right :seq2: :succeed: pars))
-#||#
 
 (define (:alt: . pars)
   (define ((:alt2: par1 par2) in ok bad)
@@ -220,7 +132,6 @@ Los combinadores de parsers son procedimientos que toman como argumentos a parse
                                           (why-failed f2))
                                   in)))))))
   (fold-right :alt2: :fail: pars))
-#||#
 
 (define (:*: par)
   (define (par* in ok bad)
@@ -233,20 +144,9 @@ Los combinadores de parsers son procedimientos que toman como argumentos a parse
          (lambda (f1)
            (:succeed: in ok bad))))
   par*)
-#||#
 
 (define (:+: par)
   (:seq: par (:*: par)))
-#||#
-
-#|
-
-### ***Metacombinadores***
-
-`((:><: fmatch fwhy) par)`
- : Regresa un combinador que utiliza `par` sobre la entrada y aplica `fmatch` al `match` de un éxito y `fwhy` al `why` de un fallo.
-
-|#
 
 (define (((:><: fmatch fwhy) par) in ok bad)
   (par in
@@ -256,54 +156,34 @@ Los combinadores de parsers son procedimientos que toman como argumentos a parse
        (lambda (f)
          (bad (failure (fwhy (why-failed f))
                        (pending-input f))))))
-#||#
-
-#|
-
-### ***Resultados de parseo***
-
-|#
 
 (define (success match pending)
-  `(success ,match ,pending))
+  (vector 'success match pending))
 
 (define (success? x)
-  (and (list? x)
-       (= 3 (length x))
-       (eq? 'success (car x))))
-#||#
+  (and (vector? x)
+       (= 3 (vector-length x))
+       (eq? 'success (vector-ref x 0))))
 
 (define (failure why pending)
-  `(failure ,why ,pending))
+  (vector 'failure why pending))
 
 (define (failure? x)
-  (and (list? x)
-       (= 3 (length x))
-       (eq? 'failure (car x))))
-#||#
+  (and (vector? x)
+       (= 3 (vector-length x))
+       (eq? 'failure (vector-ref x 0))))
 
 (define (success-match s)
   (assert (success? s))
-  (cadr s))
+  (vector-ref s 1))
 
 (define (why-failed f)
   (assert (failure? f))
-  (cadr f))
+  (vector-ref f 1))
 
 (define (pending-input r)
   (assert (or (success? r) (failure? r)))
-  (caddr r))
-#||#
-
-#|
-
-### ***Macros***
-
-Se utilizan los macros `parser` y `define-parser` para admitir la definición recursiva de parsers. Si estas formas no son utilizadas se pueden encontrar errores, considera:
-
-`> (define foo (:alt: :empty: (:seq: (:eqv: 1) foo)))`
-
-|#
+  (vector-ref r 2))
 
 (define-syntax parser
   (syntax-rules ()
@@ -315,4 +195,3 @@ Se utilizan los macros `parser` y `define-parser` para admitir la definición re
   (syntax-rules ()
     ((define-parser name par)
      (define name (parser par)))))
-#||#
